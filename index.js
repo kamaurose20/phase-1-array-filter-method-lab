bd407057dabb065d6e3cba19ab5fbbaf7644a5ab
rosekamau <kamaurose236@gmail.com>
2024-07-11 23:07:34 +0300

    array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const drivers = ['Marry', 'Tammy', 'Cullen', 'Annex', 'Brenda', 'Tobby']
+
+drivers.length = 0;
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+drivers.length = 0;
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+drivers.length = 0;
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
